b8f1ef7717f69d666cf552f8154f38481c8984f5
Sabina Kiss <kisssabina@gmail.com>
2024-07-03 18:07:38 +0200

    more project structure, adding favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+
 export default function HomePage() {
   return (
-    <main>
+    <>
       <div className="flex items-center justify-center h-screen">
         <h1 className="text-6xl font-bold">Hello, Gameshow!</h1>
       </div>
-    </main>
+      <div className="flex items-center justify-center h-screen">
+        <button>Get Started</button>
+      </div>
+    </>
   );
 }
