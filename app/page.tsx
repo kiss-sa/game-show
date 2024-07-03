2b573d406aa8d731567f8dc093b9e4486ae7c07a
Sabina Kiss <kisssabina@gmail.com>
2024-07-03 17:02:48 +0200

    add next.js project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-6xl font-bold">Hello, Gameshow!</h1>
+      </div>
+    </main>
+  );
+}
